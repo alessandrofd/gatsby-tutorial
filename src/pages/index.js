eb9a5c45e433b8edd4abda3523f6df4bd2faf65b
Alessandro Dantas <alessandrofd@yahoo.com.br>
2020-11-11 22:01:35 -0300

    9-global-css

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,10 @@ import Layout from "../components/Layout"
 
 export default () => (
   <Layout>
-    <h1 style={{ color: "rede", textTransform: "uppercase" }}>
+    <h1 style={{ color: "green", textTransform: "uppercase" }}>
       Hello from Gatsby!
     </h1>
+    <h1>Hello Word!</h1>
+    <h1>Hello People!</h1>
   </Layout>
 )
