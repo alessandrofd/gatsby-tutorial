b3b7bb76ea83ce9a645c910ed08f8542d1f01224
Alessandro Dantas <alessandrofd@yahoo.com.br>
2020-11-11 21:53:29 -0300

    6-layout-component

---
+++ src/pages/products.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const products = () => {
+  return (
+    <Layout>
+      <h1>This is our products page</h1>
+    </Layout>
+  )
+}
+
+export default products
