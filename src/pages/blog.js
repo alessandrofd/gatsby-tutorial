9341c6c9744216f496bea52cdb7a75cda814a350
Alessandro Dantas <alessandrofd@yahoo.com.br>
2020-11-11 22:10:19 -0300

    10-naming-issue

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,6 +5,12 @@ const blog = () => {
   return (
     <Layout>
       <h1>This is our blog page.</h1>
+      <p className="blog-text">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam natus
+        nam nemo commodi nobis fuga sit iure adipisci officiis deleniti. Lorem
+        ipsum dolor sit amet consectetur, adipisicing elit. Quia, sit. Lorem
+        ipsum dolor sit, amet consectetur adipisicing elit. Rerum, facilis?
+      </p>
     </Layout>
   )
 }
--- src/pages/products.js
+++ src/pages/products.js
@@ -5,6 +5,12 @@ const products = () => {
   return (
     <Layout>
       <h1>This is our products page</h1>
+      <p className="products-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus,
+        similique perferendis? Magnam omnis voluptate cum consequuntur. Totam
+        natus reiciendis aliquam animi vero, veritatis modi earum minima
+        corporis cumque cum debitis!
+      </p>
     </Layout>
   )
 }
