8b7a811ffb84a204036550230db69f0308f1b94c
Alessandro Dantas <alessandrofd@yahoo.com.br>
2020-11-12 10:44:16 -0300

    26-page-query

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -1,16 +1,42 @@
 import React from "react"
+import { graphql } from "gatsby"
+
 import Layout from "../components/Layout"
 import Header from "../examples/Header"
 import HeaderStatic from "../examples/HeaderStatic"
 
-const examples = () => {
+const examples = ({ data }) => {
+  const {
+    site: {
+      info: { author },
+    },
+  } = data
+
   return (
     <Layout>
       <h1>This is our Examples page.</h1>
       <Header />
       <HeaderStatic />
+      <h5>author: {author}</h5>
     </Layout>
   )
 }
+
+export const data = graphql`
+  {
+    site {
+      info: siteMetadata {
+        title
+        description
+        author
+        data
+        person {
+          name
+          age
+        }
+      }
+    }
+  }
+`
 
 export default examples
